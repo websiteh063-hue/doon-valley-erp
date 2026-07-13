8ab808c132dafc42b283f295d01506ae6c11af02
websiteh063-hue <websiteh063@gmail.com>
2026-07-13 10:55:20 +0530

    fix: pre-save password hook signature for modern mongoose

--- server/models/User.js
+++ server/models/User.js
@@ -57,15 +57,10 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Compare password method
